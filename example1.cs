76293bc60a2fcccbcabfbf45aa0e3391aefb6e05
Devarla Anusha <devarlaanusha8978@gmail.com>
2023-06-18 23:01:11 +0530

    commiting example1 to master

---
+++ example1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		static void Main()
+        {
+			int num;
+			Console.WriteLine("enter number where to stop");
+			num = Convert.ToInt32(Console.ReadLine());
+			for (int i=0;i>=num;i++)
+            {
+				Console.WriteLine(i);
+            }
+        }
+	}
+}
